87aa1cfd103619c88aabbe6267ab61370c1634b8
Charles Mejilla <cmejilla@gmail.com>
2016-09-24 15:26:27 +0200

    Added devise user registration, login, log out, flash messages, and tweaked UI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note to set this to your actual host (www.site.com)
+  config.action_mailer.default_url_options = { host: 'http://ruuchuck.herokuapp.com/' }
 end
